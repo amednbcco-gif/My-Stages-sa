ec4ce13cb9892e485e9f9b61ccb7915f9ebfe659
Bachir Hamoud <bachiralarhabi@gmail.com>
2026-09-06 22:48:45 +0300

    Updated ProjectDetailScreen.tsx

--- src/screens/ProjectDetailScreen.tsx
+++ src/screens/ProjectDetailScreen.tsx
@@ -401,7 +401,7 @@ function PermitTable({ permits, onPermitAdd, onPermitUpdate, onPermitDelete, can
       <div className="overflow-x-auto -mx-2 px-2">
         <table className="w-full min-w-[820px] border-collapse text-xs">
           <thead>
-            <tr className="border-b border-ink-600 text-gray-500">
+                        <tr className="border-b border-ink-600 text-gray-500">
               <th className="py-2 px-2 text-left font-semibold">SN</th>
               <th className="py-2 px-2 text-left font-semibold">Permit No</th>
               <th className="py-2 px-2 text-left font-semibold">Issued Date</th>
@@ -409,6 +409,7 @@ function PermitTable({ permits, onPermitAdd, onPermitUpdate, onPermitDelete, can
               <th className="py-2 px-2 text-left font-semibold">End Date</th>
               <th className="py-2 px-2 text-left font-semibold">CW (m)</th>
               <th className="py-2 px-2 text-left font-semibold">Permit Status</th>
+              <th className="py-2 px-2 text-left font-semibold min-w-[180px]">Note</th>
               <th className="py-2 px-2 text-center font-semibold w-8"></th>
             </tr>
           </thead>
