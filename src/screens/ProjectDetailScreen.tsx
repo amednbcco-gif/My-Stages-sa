f93d72de6d769962e9d61f20b1a10e1cf2c44791
Bachir Hamoud <bachiralarhabi@gmail.com>
2026-09-10 13:08:29 +0300

    Updated ProjectDetailScreen.tsx

--- src/screens/ProjectDetailScreen.tsx
+++ src/screens/ProjectDetailScreen.tsx
@@ -580,9 +580,9 @@ function MilestoneCard({
       </div>
 
       {/* Fields grid OR permit table */}
-                {milestone.id === "permit" ? (
-        <PermitTable permits={permits} onPermitAdd={onPermitAdd} onPermitUpdate={onPermitUpdate} onPermitDelete={onPermitDelete} canEdit={canEdit} />
-      ) : (
+                      {milestone.id === "permit" ? (
+        <PermitTable permits={permits} onPermitAdd={onPermitAdd} onPermitUpdate={onPermitUpdate} onPermitDelete={onPermitDelete} canEdit={fieldsEditable} />
+          ) : (
         <div className="px-5 py-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-3">
           {milestone.fields.map((field) => {
             let value = stageData[field.key] ?? "";
