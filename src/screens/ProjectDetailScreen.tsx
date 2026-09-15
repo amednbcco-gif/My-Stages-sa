9fc93b8cb9c69b204f9af1b2780590724cce723c
Bachir Hamoud <bachiralarhabi@gmail.com>
2026-09-15 12:30:49 +0300

    Updated ProjectDetailScreen.tsx

--- src/screens/ProjectDetailScreen.tsx
+++ src/screens/ProjectDetailScreen.tsx
@@ -669,6 +669,7 @@ function MilestoneCard({
                     </div>
                   </div>
                   <div className="ml-[8.5rem] flex flex-wrap gap-2">
+                     </a>
    href="https://intranet/tech/txm/GIS"
                       target="_blank"
                       rel="noopener noreferrer"
